4f87752669c46d4017689dc73c382b5e5c7d3329
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-08-16 00:22:16 +0300

    Update some tests

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -1,16 +1,27 @@
 import json
+import logging
 from pathlib import Path
 
+import allure
 import requests
+from allure_commons.types import AttachmentType
 from jsonschema import validate
 
 import tests
 
 url = "https://reqres.in/api/users"
 
-# response = requests.request("POST", url, data=payload)
-# print(response.text)
 
+def reqres_api_get(endpoint, **kwargs):
+    with allure.step('API Request'):
+        result = requests.get(url='https://reqres.in' + endpoint, **kwargs)
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True),
+                      name="Response", attachment_type=AttachmentType.JSON, extension='json')
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
+
+    return result
 
 def file_path(file_name):
     return str(Path(tests.__file__).parent.parent.joinpath(f'schemas/{file_name}').absolute())
@@ -23,7 +34,7 @@ def test_api():
     assert response.status_code == 201
 
     body = response.json()
-    with open(file_path("../resources/post_users.json")) as file:
+    with open(file_path("post_users.json")) as file:
         validate(body, schema=json.load(file))
 
 
@@ -42,13 +53,17 @@ def test_job_name_from_request_returns_in_response():
     assert body['job'] == job
 
 
-def test_get():
+def test_list_of_users_per_page():
     '''
     Проверка на уникальность значений списка id
     '''
-    response = requests.get(url='https://reqres.in/api/users',
-                            params={'page': 2, 'per_page': 4})
-    ids = [elem['id'] for elem in response.json()['data']]
-    assert len(ids) == len(set(ids))
+    page = 2
+    per_page = 6
+    endpoint = '/api/users'
+
+    response = reqres_api_get(endpoint=endpoint, params={'page': page, 'per_page': per_page})
+
+    assert response.json()['per_page'] == per_page
+    assert len(response.json()['data']) == per_page
 
 
