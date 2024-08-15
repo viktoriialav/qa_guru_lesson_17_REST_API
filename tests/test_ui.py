af9170019b36f1b8b09825cf1a63d9aef6374b94
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-08-16 00:22:00 +0300

    Add new test for UI with api and without

---
+++ tests/test_ui.py
@@ -0,0 +1,50 @@
+from time import sleep
+
+import requests
+from allure_commons._allure import step
+from allure_commons.types import AttachmentType
+from selene import browser
+from selene.support.conditions import have
+import allure
+
+LOGIN = "example1200@example.com"
+PASSWORD = "123456"
+WEB_URL = "https://demowebshop.tricentis.com/"
+API_URL = "https://demowebshop.tricentis.com/"
+
+
+def test_login():
+    """Successful authorization to some demowebshop (UI)"""
+    with step("Open login page"):
+        browser.open("http://demowebshop.tricentis.com/login")
+
+    with step("Fill login form"):
+        browser.element("#Email").send_keys(LOGIN)
+        browser.element("#Password").send_keys(PASSWORD).press_enter()
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
+
+
+def test_login_through_api():
+    """Successful authorization to some demowebshop (UI)"""
+    with allure.step('Login with API'):
+        result = requests.post(url=API_URL + '/login',
+                               data={'Email': LOGIN, 'Password': PASSWORD,  'RememberMe': False},
+                               allow_redirects=False)
+        allure.attach(body=result.text, name="Response", attachment_type=AttachmentType.TEXT, extension='txt')
+        allure.attach(body=str(result.cookies), name="Cookies", attachment_type=AttachmentType.TEXT, extension='txt')
+
+    with allure.step('Get cookie from API'):
+        cookie = result.cookies.get('NOPCOMMERCE.AUTH')
+
+    with allure.step('Set cookie from API'):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({'name': 'NOPCOMMERCE.AUTH', 'value': cookie})
+        browser.open(WEB_URL)
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
+
+
+
